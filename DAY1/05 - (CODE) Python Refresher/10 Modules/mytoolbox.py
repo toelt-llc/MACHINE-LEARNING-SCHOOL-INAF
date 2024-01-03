f73d2a7a16bcbebe12b4b35c189e419c3c6788a3
Umberto Michelucci <umberto.michelucci@toelt.ai>
2024-01-03 21:18:12 +0100

    Updated hands-on

---
+++ DAY1/05 - (CODE) Python Refresher/10 Modules/mytoolbox.py
@@ -0,0 +1,4 @@
+def headline(msg, n_stars=40):
+    print("*" * n_stars)
+    print(msg)
+    print("*" * n_stars)
--- DAY1/06 - Python Refresher/10 Modules/mytoolbox.py
+++
@@ -1,4 +0,0 @@
-def headline(msg, n_stars=40):
-    print("*" * n_stars)
-    print(msg)
-    print("*" * n_stars)
